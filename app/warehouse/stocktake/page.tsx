e907a7e4d250a7761ba479b8264c708eb60e43d6
MisDieRekt <52544802+MisDieRekt@users.noreply.github.com>
2024-07-05 15:00:04 +0200

    hmmm

--- app/warehouse/stocktake/page.tsx
+++ app/warehouse/stocktake/page.tsx
@@ -2,7 +2,7 @@
 
 import React, { useEffect, useRef, useState } from "react";
 import { Html5Qrcode, CameraDevice } from "html5-qrcode";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 import { createClient } from "@/utils/supabase/client";
 
 interface StockInfo {
