65639a1105e71fe37d9da2fae7b324d4343d3f9f
MisDieRekt <52544802+MisDieRekt@users.noreply.github.com>
2024-07-18 13:53:23 +0200

    fontsizes
    updates layout - font sizes

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -2,16 +2,22 @@ import Link from "next/link";
 
 const Footer: React.FC = () => {
   return (
-    <footer className="w-full border-t border-t-foreground/10 py-4">
+    <footer className="w-full border-t border-t-foreground/10 py-4 no-print">
       <div className="w-full max-w-4xl mx-auto flex flex-col sm:flex-row justify-between items-center p-3 text-sm">
         <div className="flex flex-col sm:flex-row items-center">
           <Link href="/" className="text-foreground hover:text-blue-500 mx-2">
             Home
           </Link>
-          <Link href="/about" className="text-foreground hover:text-blue-500 mx-2">
+          <Link
+            href="/about"
+            className="text-foreground hover:text-blue-500 mx-2"
+          >
             About
           </Link>
-          <Link href="/contact" className="text-foreground hover:text-blue-500 mx-2">
+          <Link
+            href="/contact"
+            className="text-foreground hover:text-blue-500 mx-2"
+          >
             Contact
           </Link>
         </div>
--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import DeployButton from './DeployButton';
-import AuthButton from './AuthButton';
+import React from "react";
+import DeployButton from "./DeployButton";
+import AuthButton from "./AuthButton";
 
 interface NavBarProps {
   isSupabaseConnected: boolean;
@@ -8,7 +8,7 @@ interface NavBarProps {
 
 const NavBar: React.FC<NavBarProps> = ({ isSupabaseConnected }) => {
   return (
-    <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
+    <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16 no-print">
       <div className="w-full max-w-4xl flex justify-between items-center p-3 text-sm">
         <DeployButton />
         {isSupabaseConnected && <AuthButton />}
